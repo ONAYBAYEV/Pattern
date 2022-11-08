d00a6f4bcc0592317c8bce526389abb13265e7de
Meyirbek <meiirbek.ongaibay@narxoz.kz>
2022-11-08 22:37:47 +0600

    first commit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,12 +19,13 @@
                             <div class="card-header" ><h5 class="card-title">{{$post->title}} </h5></div>
                             <img class="card-img-top" src="{{$post->image}}" alt="Card image cap">
                             <p class="card-text">{{$post->content}}</p>
-
+                            @can('delete',$post)
                                  <form action="{{route('posts.destroy',$post->id)}}"method="post">
                                     @csrf
                                     @method('DELETE')
                                     <button class="btn btn-danger" type="submit">Delete</button>
                                 </form>
+                            @endcan
                                 <button type="button" class="btn btn-success"><a href="{{route('posts.show',$post->id)}}" >read more</a></button>
 
                             <span class="d-block pb-sm-1 text-bg-primary"><small>{{$post->user->name}}</small></span>
